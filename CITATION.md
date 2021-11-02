80a24d8e0d2fed73f761f43b44d8b886dc610530
Laurens Stoop MAC13 <l.p.stoop@uu.nl>
2021-11-02 10:54:42 +0100

    First

---
+++ CITATION.md
@@ -0,0 +1,3 @@
+Please cite this project as follows:
+
+Laurens Stoop (2021),  WeatherRegimes - version 0.1.0. url: github.com/laurensstoop/weatherregimes
---
+++ src/FalkenaData.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  2 09:46:19 2021
+
+@author: Laurens Stoop
+"""
+
+# load the dependencies
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+
+# directory definitions
+# os.chdir('/Users/stoop/Documents/Projects/WeatherRegimes/')
+
+
+#%%
+# =============================================================================
+# Loading the data
+# =============================================================================
+
+
+# with numpy.load('foo.npz') as data:
+#     a = data['a']
